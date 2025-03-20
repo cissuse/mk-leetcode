94aa6202f4126f2b25cd8c9be5afce8954fea5f9
mk <moonykun.k.yang@foxconn.com>
2025-03-20 10:04:42 +0800

    🎉 init: first commit

---
+++ 1.两数之和.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        return new int[0];
+    }
+}
+// @lc code=end
+
